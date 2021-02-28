9a99d2a993217a4f4f775b5e32c380f9964689fe
FernandoProfessional <79805224+FernandoProfessional@users.noreply.github.com>
2021-02-28 19:35:14 +0100

    Add files via upload

---
+++ src/modelo/beans/Libro.java
@@ -0,0 +1,86 @@
+package modelo.beans;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+
+/**
+ * The persistent class for the libros database table.
+ * 
+ */
+@Entity
+@Table(name="libros")
+@NamedQuery(name="Libro.findAll", query="SELECT l FROM Libro l")
+public class Libro implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private long isbn;
+
+	private String autor;
+
+	@Column(name="PRECIO_UNITARIO")
+	private BigDecimal precioUnitario;
+
+	private int stock;
+
+	private String titulo;
+
+	//uni-directional many-to-one association to Tema
+	@ManyToOne
+	@JoinColumn(name="ID_TEMA")
+	private Tema tema;
+
+	public Libro() {
+	}
+
+	public long getIsbn() {
+		return this.isbn;
+	}
+
+	public void setIsbn(long isbn) {
+		this.isbn = isbn;
+	}
+
+	public String getAutor() {
+		return this.autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public BigDecimal getPrecioUnitario() {
+		return this.precioUnitario;
+	}
+
+	public void setPrecioUnitario(BigDecimal precioUnitario) {
+		this.precioUnitario = precioUnitario;
+	}
+
+	public int getStock() {
+		return this.stock;
+	}
+
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+
+	public String getTitulo() {
+		return this.titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public Tema getTema() {
+		return this.tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
+
+}
